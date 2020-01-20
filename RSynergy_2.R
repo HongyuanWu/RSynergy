dc02917cd5aa87d7cf53df9590ec3b199fd45233
Tamas Ferenci <ferenci@research.physcon.uni-obuda.hu>
2020-01-20 15:20:22 +0100

    Major upgrade: - Thanks to the new version of synergyfinder, we no longer need custom-built solutions, we can use the package's functions. - Started the implementation of reproducing cell line descriptors as well.

--- RSynergy_2.R
+++ RSynergy_2.R
@@ -1,4 +1,4 @@
-library( data.table )
+library(data.table)
 
 # We read the data for the combinations
 RawDataComb <- data.table( rio::import( paste0( "https://mct.aacrjournals.org/highwire/filestream/53222/",
@@ -15,31 +15,28 @@ xtabs(~Batch1+Batch2+Batch3,data=RawDataComb[,as.list(table(factor(BatchID,level
 # BatchID can be safely dropped (as there is no documentation on any difference between them).
 RawDataComb <- RawDataComb[ BatchID!=3 ]
 
+# Percentage inhibition will be needed later
+RawDataComb$response <- 100-100*RawDataComb$X.X0
+
 # We read the single agent-data
 RawDataSingle <- data.table( rio::import( paste0( "https://mct.aacrjournals.org/highwire/filestream/53222/",
                                                   "field_highwire_adjunct_files/1/156849_1_supp_0_w2lh45.xlsx" ),
                                           format = "xlsx", na = "NULL", .name_repair = "universal" ) )
+RawDataSingle$response <- 100-100*RawDataSingle$X.X0 # Percentage inhibition will be needed later
+RawDataSingle$dose <- RawDataSingle$Drug_concentration..µM. # To simplify feeding data into synergyfinder
 
-# Custom script derived from synergyfinder to calculate the synergyscores (until it is figured out how to do this
-# with synergyscore itself)
-source( "Loewe2.R" )
-
-# A check to see if Loewe2 is OK
-Loewe2( RawDataComb[drugA_name=="5-FU"&drugB_name=="ABT-888"&cell_line=="A2058",
-                    .(conc_r=drugA.Conc..µM.,conc_c=drugB.Conc..µM.,response=X.X0*100,cell_line=cell_line,block_id=1,
-                      drug_row="5-FU",drug_col="ABT-888",conc_r_unit="µM",conc_c_unit="µM")],
-        RawDataSingle[drug_name=="5-FU"&cell_line=="A2058",.(dose=Drug_concentration..µM.,response=100-X.X0*100)],
-        RawDataSingle[drug_name=="ABT-888"&cell_line=="A2058",.(dose=Drug_concentration..µM.,response=100-X.X0*100)] )
-
-# We transform the data to the format that is required by the synergyfinder/Loewe2
-ChData <- RawDataComb[,.(conc_r=drugA.Conc..µM.,conc_c=drugB.Conc..µM.,response=X.X0*100,drug_row=drugA_name,
-                         drug_col=drugB_name,conc_c_unit="µM",conc_r_unit="µM",cell.line=cell_line,block_id=1)]
-
-# Calculate alls synergy scores with our method
-res <- ChData[,.(Loewe2(cbind(.SD,drug_row,drug_col),
-                 RawDataSingle[drug_name==drug_row&cell_line==cell.line,.(dose=Drug_concentration..µM.,response=100-X.X0*100)],
-                 RawDataSingle[drug_name==drug_col&cell_line==cell.line,.(dose=Drug_concentration..µM.,response=100-X.X0*100)])),
-       .(drug_row,drug_col,cell.line) ]
+# These will be repeatedly needed, so calculate them a single time
+RawDataSingleModels <- lapply( setNames(unique(RawDataSingle$drug_name), unique(RawDataSingle$drug_name)),
+                               function(drug) lapply( setNames(unique(RawDataSingle$cell_line), unique(RawDataSingle$cell_line)),
+                                                      function(cline) synergyfinder::FitDoseResponse(
+                                                        RawDataSingle[drug_name==drug&cell_line==cline] ) ) )
+# Calculating all synergy scores
+res <- RawDataComb[ ,mean( synergyfinder::Loewe( as.matrix( dcast(.SD, drugA.Conc..µM.~drugB.Conc..µM., value.var = "response"),
+                                                            rownames = 1 ),
+                                                 drug.row.model = RawDataSingleModels[[drugA_name]][[cell_line]],
+                                                 drug.col.model = RawDataSingleModels[[drugB_name]][[cell_line]] )[-1,-1] ),
+                    .(drugA_name,drugB_name,cell_line) ]
+names(res)[1:3] <- c( "drug_row", "drug_col", "cell.line" )
 
 # Let's save the data so that later we can skip this time-consuming data preparation
 saveRDS( res, "resRSynergy.dat" )
@@ -49,9 +46,9 @@ labels <- read.csv( "http://www.bioinf.jku.at/software/DeepSynergy/labels.csv" )
 names( labels )[ 2:3 ] <- c( "drug_row", "drug_col" )
 
 # ...and check how similar it is to our ones
-plot( V1 ~ synergy, data = merge( res[ , .( drug_row=toupper(drug_row), drug_col=toupper(drug_col),
-                                            cell_line=toupper(cell.line), V1 ) ], labels ) )
-abline( 0, 1 )
+plot( V1 ~ synergy, data = merge( res[ , .( drug_row=toupper(drugA_name), drug_col=toupper(drugB_name),
+                                            cell_line=toupper(cell_line), V1 ) ], labels ) )
+abline( 0, 1, col = "red" )
 
 # Plot something like their Figure 1
 lattice::levelplot( V1~factor(drug_row)+factor(drug_col), data = res[cell.line=="ZR751"],
@@ -74,6 +71,21 @@ lattice::levelplot( V1~(drug_row)+as.factor(drug_col),
                     scales = list( x = list( rot = 90 ) ), xlab = "", ylab = "", main = "ZR751",
                     col.regions = colorRampPalette(c("darkred","darkblue"))(1000))
 
+# We now load the descriptors of the cell lines from ArrayExpress E-MTAB-3610, as described in the paper
+
+CellLineData <- ArrayExpress::getAE( "E-MTAB-3610" )
+CellLineData <- affy::ReadAffy( filenames = CellLineData$rawFiles[ match(
+  read.AnnotatedDataFrame( CellLineData$sdrf )@data$Array.Data.File,CellLineData$rawFiles ) ],
+  phenoData = CellLineData$sdrf,
+  description = CellLineData$idf )
+CellLineData <- CellLineData[ , toupper( gsub( "-", "", CellLineData@phenoData@data$Characteristics.cell.line. ) )%in%
+                                unique( res$cell.line ) ]
+# Only 28 is found from 39!!! (e.g. CAOV3)
+CellLineData <- farms::qFarms( CellLineData )
+CellLineData <- farms::INIcalls( CellLineData )
+summary( CellLineData )
+plot( CellLineData )
+CellLineData <- farms::getI_Eset( CellLineData )
 
 # We now load the descriptors of the drugs and the cell lines, as provided by the original group
 download.file( "http://www.bioinf.jku.at/software/DeepSynergy/X.p.gz", "X.p.gz", mode = "wb" )
